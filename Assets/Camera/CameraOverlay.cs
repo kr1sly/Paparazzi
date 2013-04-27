7d2e876e61051542ba892effb50f20d51ad62522
Christoph <chrisslybaer@gmx.net>
2013-04-27 21:04:41 +0200

    stuff

--- Assets/Camera/CameraOverlay.cs
+++ Assets/Camera/CameraOverlay.cs
@@ -6,11 +6,17 @@ using System.Collections;
 public class CameraOverlay: MonoBehaviour
 {
 	private GameObject cameraOverlay;
+	private GameObject intro1;
+	private GameObject intro2;
 	
 	public void Start()
 	{
 		cameraOverlay = GameObject.Find ("cameraOverlay");
+		intro1 = GameObject.Find ("intro1");
+		intro2 = GameObject.Find ("intro2");
 		cameraOverlay.SetActive(false);
+		intro1.SetActive(false);
+		intro2.SetActive(false);
 	}
 	
 	public void OnEnable ()
@@ -22,6 +28,16 @@ public class CameraOverlay: MonoBehaviour
 	
 	public void Update()
 	{
+		if (Time.frameCount <= 100)
+		{
+			intro1.SetActive (true);
+		}
+		else if (Time.frameCount <= 200)
+		{
+			intro1.SetActive (false);
+			intro2.SetActive (true);
+		}
+		else intro2.SetActive (false);
 		if(Input.GetKeyDown (KeyCode.C))
 		{
 			cameraOverlay.SetActive(!cameraOverlay.activeSelf);
